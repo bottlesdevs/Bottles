c82397db3752fd59bce58b01509c4271d30871e5
Mirko Brombin <brombinmirko@gmail.com>
2017-10-19 18:42:54 +0200

    Changed primaryColor

--- bottles/constants.py
+++ bottles/constants.py
@@ -37,6 +37,6 @@ class App:
     about_license_type = Gtk.License.GPL_3_0
 
 class Colors:
-    primary_color = "#2d2227"
+    primary_color = "#3a3336"
     primary_text_color = "#f0e7e7"
     primary_text_shadow_color = "#160404"
