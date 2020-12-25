b0278db602ecd4221a81ad16c9154691bbe1339f
Mirko Brombin <send@mirko.pm>
2020-12-25 16:15:30 +0100

    Create importer.py page

---
+++ src/pages/importer.py
@@ -0,0 +1,42 @@
+# importer.py
+#
+# Copyright 2020 brombinmirko <send@mirko.pm>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from gi.repository import Gtk
+
+@Gtk.Template(resource_path='/com/usebottles/bottles/importer.ui')
+class BottlesImporter(Gtk.Box):
+    __gtype_name__ = 'BottlesImporter'
+
+    '''
+    Get and assign widgets to variables from
+    template childs
+    '''
+    list_prefixes = Gtk.Template.Child()
+
+    def __init__(self, window, **kwargs):
+        super().__init__(**kwargs)
+
+        '''
+        Initialize template
+        '''
+        self.init_template()
+
+        '''
+        Set reusable variables
+        '''
+        self.window = window
+        self.runner = window.runner
