bf26900353ceb3af34b7dc350dfe08441b419761
Mirko Brombin <send@mirko.pm>
2021-01-03 11:11:03 +0100

    Better use of comments in params.py

--- src/params.py
+++ src/params.py
@@ -1,17 +1,10 @@
-'''
-General application details
-'''
+'''Application details'''
 APP_NAME = "Bottles"
 APP_NAME_LOWER = APP_NAME.lower()
 APP_ID = "com.usebottles.bottles"
 
-'''
-These parameters cannot be changed from the user settings.
-'''
-
+'''Internal settings not user editable'''
 ANIM_DURATION = 120
 
-'''
-These below are basic parameters that the user can change from the settings.
-'''
+'''Default settings'''
 THEME_DARK = False
