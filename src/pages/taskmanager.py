87176f95188499548528af32a2eee1eea552217c
Mirko Brombin <send@mirko.pm>
2021-01-03 12:02:23 +0100

    Better comments usage in taskmanager.py

--- src/pages/taskmanager.py
+++ src/pages/taskmanager.py
@@ -21,54 +21,40 @@ from gi.repository import Gtk
 class BottlesTaskManager(Gtk.Box):
     __gtype_name__ = 'BottlesTaskManager'
 
-    '''
-    Get and assign widgets to variables from
-    template childs
-    '''
+    '''Get widgets from template'''
     treeview_processes = Gtk.Template.Child()
     btn_processes_update = Gtk.Template.Child()
 
     def __init__(self, window, **kwargs):
         super().__init__(**kwargs)
 
-        '''
-        Initialize template
-        '''
+        '''Init template'''
         self.init_template()
 
-        '''
-        Set reusable variables
-        '''
+        '''Common variables'''
         self.window = window
         self.runner = window.runner
 
-        '''
-        Create a liststore to store processes data
-        '''
+        '''Apply model to treeview_processes'''
         self.liststore_processes = Gtk.ListStore(str, str, str, str, str, str)
         self.treeview_processes.set_model(self.liststore_processes)
 
         cell_renderer = Gtk.CellRendererText()
         i = 0
 
-        '''
-        Add columns to treeview
-        '''
+        '''Add columns to treeview_processes'''
         for column in ["PID", "Memory", "CPU", "Start", "Time", "Command"]:
             column = Gtk.TreeViewColumn(column, cell_renderer, text=i)
             self.treeview_processes.append_column(column)
             i += 1
 
-        '''
-        Connect signals to widgets
-        '''
+        '''Signal connections'''
         self.btn_processes_update.connect('pressed', self.update_processes)
 
+        '''Run updates'''
         self.update_processes()
 
-    '''
-    Check for processed and populate the treeview
-    '''
+    '''Populate liststore_processes'''
     def update_processes(self, widget=False):
         self.liststore_processes.clear()
         processes = self.runner.get_running_processes()
