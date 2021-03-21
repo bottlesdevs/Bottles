5d6f4958cf3598dd007497e138e71feb98b34e3f
Mirko Brombin <send@mirko.pm>
2021-03-21 10:09:35 +0100

    Typo

--- src/window.py
+++ src/window.py
@@ -150,7 +150,7 @@ class BottlesWindow(Handy.ApplicationWindow):
         self.on_start()
 
         if arg_executable:
-            if arg_executable.endswith(('.exe', '.msi', '.bat'))
+            if arg_executable.endswith(('.exe', '.msi', '.bat')):
                 self.show_list_view()
 
         '''Toggle view_switcher_bar by window size'''
