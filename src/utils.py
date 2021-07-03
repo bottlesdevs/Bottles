c9d449be2e164b196974bb8c7860e027ba1f07a2
mirkobrombin <send@mirko.pm>
2021-07-03 21:34:36 +0200

    validate_url method

--- src/utils.py
+++ src/utils.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import re
 import sys
 import shutil
 import logging
@@ -236,3 +237,13 @@ class CabExtract():
         return False
 
         
+def validate_url(url: str):
+    regex = re.compile(
+        r'^(?:http|ftp)s?://'
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+        r'localhost|'
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+        r'(?::\d+)?'
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    return re.match(regex, "http://www.example.com") is not None
