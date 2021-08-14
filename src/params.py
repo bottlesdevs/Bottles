81da41b6ad2ad0afa4e58fe243dd1780ba0bf8a8
mirkobrombin <send@mirko.pm>
2021-08-14 08:37:17 +0200

    release 2021.8.14

--- src/params.py
+++ src/params.py
@@ -2,7 +2,7 @@
 APP_NAME = "Bottles"
 APP_NAME_LOWER = APP_NAME.lower()
 APP_ID = "com.usebottles.bottles"
-VERSION = "2021.7.28-treviso-2"
+VERSION = "2021.8.14-treviso"
 
 # Internal settings not user editable
 ANIM_DURATION = 120
