bc254b0d37eab5932a41cbf4011de554479760d1
mirkobrombin <send@mirko.pm>
2021-08-30 10:28:58 +0200

    new.py/new.ui refactoring and documentation

--- src/views/new.py
+++ src/views/new.py
@@ -32,7 +32,7 @@ class EnvironmentRow(Handy.ActionRow):
         self.set_icon_name(environment.get("icon"))
         self.set_visible(True)
 
-    def get_environment_id(self):
+    def get_env_id(self):
         return self.environment.get("id")
 
 @Gtk.Template(resource_path='/com/usebottles/bottles/new.ui')
@@ -46,7 +46,7 @@ class NewView(Handy.Window):
     btn_close = Gtk.Template.Child()
     btn_pref_runners = Gtk.Template.Child()
     btn_pref_dxvk = Gtk.Template.Child()
-    list_environments = Gtk.Template.Child()
+    list_envs = Gtk.Template.Child()
     page_create = Gtk.Template.Child()
     page_creating = Gtk.Template.Child()
     page_created = Gtk.Template.Child()
@@ -86,56 +86,62 @@ class NewView(Handy.Window):
         super().__init__(**kwargs)
         self.set_transient_for(window)
 
-        '''Common variables'''
+        # common variables and references
         self.window = window
         self.manager = window.manager
         self.arg_exe = arg_exe
         self.arg_lnk = arg_lnk
-        self.selected_environment = "gaming"
+        self.selected_env = "gaming"
         self.new_bottle_config = {}
 
-        '''Signal connections'''
-        self.btn_cancel.connect('pressed', self.close_window)
-        self.btn_close.connect('pressed', self.close_window)
+        # connect signals
+        self.btn_cancel.connect('pressed', self.__close_window)
+        self.btn_close.connect('pressed', self.__close_window)
         self.btn_create.connect('pressed', self.create_bottle)
-        self.list_environments.connect('row-selected', self.set_active_environment)
+        self.list_envs.connect('row-selected', self.set_active_env)
         self.entry_name.connect('key-release-event', self.check_entry_name)
-        self.btn_pref_runners.connect('pressed', self.window.show_preferences_view)
-        self.btn_pref_dxvk.connect('pressed', self.window.show_preferences_view)
+        self.btn_pref_runners.connect('pressed', self.window.show_prefs_view)
+        self.btn_pref_dxvk.connect('pressed', self.window.show_prefs_view)
 
-        for environment in self.environments:
-            env_row = EnvironmentRow(environment)
-            self.list_environments.add(env_row)
+        for env in self.environments:
+            env_row = EnvironmentRow(env)
+            self.list_envs.add(env_row)
 
-        '''Select the first environment in list'''
-        self.list_environments.select_row(
-            self.list_environments.get_children()[0])
+        # set the first environment as active
+        self.list_envs.select_row(self.list_envs.get_children()[0])
 
-        '''Populate combo_runner'''
+        # populate combo_runner with runner versions from the manager
         for runner in self.manager.runners_available:
             self.combo_runner.append(runner, runner)
 
         self.combo_runner.set_active(0)
 
-        '''Populate combo_dxvk'''
+        # populate combo_dxvk with dxvk versions from the manager
         for dxvk in self.manager.dxvk_available:
             self.combo_dxvk.append(dxvk, dxvk)
 
         self.combo_dxvk.set_active(0)
         self.combo_arch.set_active_id("win64")
 
-    def set_active_environment(self, widget, row):
-        '''Set selected environment'''
-        self.selected_environment = row.get_environment_id()
+    def set_active_env(self, widget, row):
+        '''
+        This function set the active environment on row selection.
+        If the environment is "Custom" it will display the advanced
+        options.
+        '''
+        self.selected_env = row.get_env_id()
 
-        '''Toggle advanced options'''
-        status = row.get_environment_id() == "Custom"
+        status = row.get_env_id() == "Custom"
         for w in [self.label_advanced, self.box_advanced]:
             w.set_visible(status)
             w.set_visible(status)
 
-    '''Validate entry_name input'''
     def check_entry_name(self, widget, event_key):
+        '''
+        This function checks if the name of the bottle is valid. So it
+        checks if the name is not empty and if it contains special
+        characters. Then it toggle the entry icon according to the result.
+        '''
         regex = re.compile('[@!#$%^&*()<>?/\|}{~:.;,\'"]')
         name = widget.get_text()
 
@@ -148,17 +154,19 @@ class NewView(Handy.Window):
 
     '''Create the bottle'''
     def create_bottle(self, widget):
-        '''Change buttons state'''
+        # set widgets states
         self.btn_cancel.set_sensitive(False)
         self.btn_create.set_visible(False)
         self.page_create.set_visible(False)
-
-        '''Show the creating page'''
         self.stack_create.set_visible_child_name("page_creating")
 
-        '''Create bottle'''
+        '''
+        Check if versioning is enabled and get the selected runner. If the
+        selected environment is not "Custom", the runner is taken from the
+        runners available list, else it is taken from the user selection.
+        '''
         versioning_state = self.switch_versioning.get_state()
-        if self.selected_environment == "Custom":
+        if self.selected_env == "Custom":
             runner = self.combo_runner.get_active_id()
         else:
             rv = [i for i in self.manager.runners_available if i.startswith('vaniglia')]
@@ -174,17 +182,22 @@ class NewView(Handy.Window):
             else: # use any other runner available
                 runner = self.manager.runners_available[0]
 
-        self.manager.create_bottle(name=self.entry_name.get_text(),
-                                  path="",
-                                  environment=self.selected_environment,
-                                  runner=runner,
-                                  dxvk=self.combo_dxvk.get_active_id(),
-                                  versioning=versioning_state,
-                                  dialog=self,
-                                  arch=self.combo_arch.get_active_id())
+        self.manager.create_bottle(
+            name=self.entry_name.get_text(),
+            path="",
+            environment=self.selected_env,
+            runner=runner,
+            dxvk=self.combo_dxvk.get_active_id(),
+            versioning=versioning_state,
+            dialog=self,
+            arch=self.combo_arch.get_active_id()
+        )
 
-    '''Concatenate label_output'''
     def idle_update_output(self, text):
+        '''
+        This function update the label_output with the givven text.
+        It will be concatenated with the previous one.
+        '''
         current_text = self.label_output.get_text()
         text = f"{current_text}{text}\n"
         self.label_output.set_text(text)
@@ -195,31 +208,46 @@ class NewView(Handy.Window):
     def finish(self, config):
         self.new_bottle_config = config
         self.page_created.set_description(
-            _("A bottle named “%s” was created successfully") % self.entry_name.get_text())
+            _("A bottle named “{0}” was created successfully").format(
+                self.entry_name.get_text()
+            )
+        )
 
         self.btn_cancel.set_visible(False)
         self.btn_close.set_visible(True)
 
         self.stack_create.set_visible_child_name("page_created")
 
-        '''Update bottles'''
+        '''
+        Ask the manager to check for new bottles, then update the
+        user bottles list. 
+        '''
         self.manager.check_bottles()
         self.window.page_list.update_bottles()
 
-    '''Progressbar pulse every 1s'''
     def pulse(self):
+        # This function update the progress bar every 1s.
         while True:
             time.sleep(1)
             self.progressbar_creating.pulse()
 
-    '''Destroy the window'''
-    def close_window(self, widget):
+    def __close_window(self, widget):
+        '''
+        This function check if an executable was passed to Bottles as
+        a command line argument. If so, it will be launched in the new
+        bottles and will close the bottle creation dialog. If there is
+        no arguments, it will simply close the dialog.
+        '''
         if self.arg_exe:
             Runner().run_executable(
                 config=self.new_bottle_config,
-                file_path=self.arg_exe)
+                file_path=self.arg_exe
+            )
+
         if self.arg_lnk is not None:
             Runner().run_lnk(
                 config=self.new_bottle_config,
-                file_path=self.arg_lnk)
+                file_path=self.arg_lnk
+            )
+
         self.destroy()
