f00eae956dcd3c6ddd3fbd81a061ab740cc09c5f
mirkobrombin <send@mirko.pm>
2021-09-10 15:09:16 +0200

    disable test assert

--- src/utils.py
+++ src/utils.py
@@ -273,7 +273,7 @@ class RunAsync(threading.Thread):
     def __init__(self, task_func, callback, *args, **kwargs):
         self.source_id = None
         self.stop_request = threading.Event()
-        assert threading.current_thread() is threading.main_thread()
+        # assert threading.current_thread() is threading.main_thread()
 
         super(RunAsync, self).__init__(
             target=self.__target, args=args, kwargs=kwargs)
