d063080bb6811d61fa299d0e9c6f1ab7cc6906d1
mirkobrombin <send@mirko.pm>
2021-10-10 17:28:31 +0200

    remove unused import

--- src/views/new.py
+++ src/views/new.py
@@ -16,7 +16,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import re
-import time
 from gettext import gettext as _
 from gi.repository import Gtk, GLib, Handy
 from ..backend.runner import Runner
