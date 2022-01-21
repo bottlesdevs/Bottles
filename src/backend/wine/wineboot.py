dcb0f27b773ebc6b593c1592cab8987827d99012
mirkobrombin <send@mirko.pm>
2022-01-21 19:24:31 +0100

    implement wineboot WineProgram

---
+++ src/backend/wine/wineboot.py
@@ -0,0 +1,60 @@
+import subprocess
+from typing import NewType
+
+from bottles.utils import UtilsLogger # pyright: reportMissingImports=false
+from bottles.backend.wine.wineprogram import WineProgram
+from bottles.backend.wine.wineserver import WineServer
+
+logging = UtilsLogger()
+
+# Define custom types for better understanding of the code
+BottleConfig = NewType('BottleConfig', dict)
+
+
+class WineBoot(WineProgram):
+    program = "WINE Runtime tool"
+    command = "wineboot"
+    
+    def send_status(self, status: int):
+        states = {
+            -1: "force",
+            0: "-k",
+            1: "-r",
+            2: "-s",
+            3: "-u",
+            4: "-i"
+        }
+        envs = {"WINEDEBUG": "-all", "DISPLAY": ":3.0"}
+        args = ""
+
+        if status == 0 and not WineServer(self.config).is_alive():
+            logging.info("There is no running wineserver.")
+            return
+        
+        if status in states:
+            args = f"{states[status]} /nogui"
+            self.launch(
+                args=args,
+                environment=envs,
+                comunicate=True
+            )
+        else:
+            raise ValueError(f"[{status}] is not a valid status for wineboot!")
+
+    def force(self):
+        return self.send_status(-1)
+    
+    def kill(self):
+        return self.send_status(0)
+
+    def restart(self):
+        return self.send_status(1)
+    
+    def shutdown(self):
+        return self.send_status(2)
+    
+    def update(self):
+        return self.send_status(3)
+    
+    def init(self):
+        return self.send_status(4)
