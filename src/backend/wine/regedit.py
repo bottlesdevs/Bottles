1b7499c08ac267bda42c0e116ca5bc6cd67176d6
mirkobrombin <send@mirko.pm>
2022-01-21 19:24:07 +0100

    implement regedit WineProgram

---
+++ src/backend/wine/regedit.py
@@ -0,0 +1,14 @@
+from typing import NewType
+
+from bottles.utils import UtilsLogger # pyright: reportMissingImports=false
+from bottles.backend.wine.wineprogram import WineProgram
+
+logging = UtilsLogger()
+
+# Define custom types for better understanding of the code
+BottleConfig = NewType('BottleConfig', dict)
+
+
+class Regedit(WineProgram):
+    program = "WINE Registry Editor"
+    command = "regedit"
