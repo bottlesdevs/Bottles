ced821b66952757b7adbed459c0bf3479b9b4d0c
Francesco Masala <mail@francescomasala.me>
2022-02-02 21:37:56 +0100

    GTK Bugfix
    Fixed error --> gi.RepositoryError: Requiring namespace 'Gtk' version '4.0', but '3.0' is already loaded

--- src/dialogs/generic.py
+++ src/dialogs/generic.py
@@ -15,6 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import gi
+
+gi.require_version('GtkSource', '4')
+
 from gi.repository import Gtk, GtkSource, Gdk, Handy, Pango, WebKit2
 
 
