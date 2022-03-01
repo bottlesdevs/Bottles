add43b4f8b23d58e05d20290d174fdd45c58d0b7
mirkobrombin <send@mirko.pm>
2022-03-01 23:13:34 +0100

    support GE-Proton runners
    ref https://github.com/bottlesdevs/components/pull/116

--- src/backend/wine/winecommand.py
+++ src/backend/wine/winecommand.py
@@ -267,7 +267,7 @@ class WineCommand:
         if runner in [None, ""]:
             return
 
-        if runner.startswith("Proton"):
+        if "Proton" in runner:
             '''
             If the runner is Proton, set the pat to /dist or /files 
             based on check if files exists.
