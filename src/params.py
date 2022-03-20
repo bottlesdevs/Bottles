f7d8f5c9a3ee9308a17383ed8488ec53ae942f28
mirkobrombin <send@mirko.pm>
2022-03-20 12:13:09 +0100

    add funding entry menu

--- src/params.py
+++ src/params.py
@@ -12,5 +12,6 @@ ANIM_DURATION = 120
 EXECUTABLE_EXTS = ('.exe', '.msi', '.bat', '.lnk')
 
 # URLs
+FUNDING_URL = "https://www.usebottles.com/funding"
 DOC_URL = "https://docs.usebottles.com"
 FORUMS_URL = "https://forums.usebottles.com"
