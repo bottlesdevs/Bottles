4d28dd86ebe6f86db604f8e07637550449a720d6
mirkobrombin <send@mirko.pm>
2022-03-28 08:59:33 +0200

    fix #1243

--- src/backend/runner.py
+++ src/backend/runner.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 from typing import NewType
 
 from bottles.utils import RunAsync  # pyright: reportMissingImports=false
