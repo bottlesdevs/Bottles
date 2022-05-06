abf83c18fe3bd27ef8e3bd48ff1b7c5edb1f4e0f
TheEvilSkeleton <theevilskeleton@riseup.net>
2022-05-06 10:59:10 -0400

    avoid renaming env vars

--- src/backend/wine/wineboot.py
+++ src/backend/wine/wineboot.py
@@ -21,7 +21,7 @@ class WineBoot(WineProgram):
             3: "-u",
             4: "-i"
         }
-        envs = {"WineDEBUG": "-all", "DISPLAY": ":3.0"}
+        envs = {"WINEDEBUG": "-all", "DISPLAY": ":3.0"}
 
         if status == 0 and not WineServer(self.config).is_alive():
             logging.info("There is no running wineserver.", )
