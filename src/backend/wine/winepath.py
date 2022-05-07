ebef44dd35b3060381ed2d32e826f6474362b226
mirkobrombin <send@mirko.pm>
2022-05-07 16:18:54 +0200

    provide native method for WinePath.to_unix() method

--- src/backend/wine/winepath.py
+++ src/backend/wine/winepath.py
@@ -2,6 +2,7 @@ from typing import NewType
 
 from bottles.backend.logger import Logger  # pyright: reportMissingImports=false
 from bottles.backend.wine.wineprogram import WineProgram
+from bottles.backend.utils.manager import ManagerUtils
 
 logging = Logger()
 
@@ -20,9 +21,17 @@ class WinePath(WineProgram):
 
     @staticmethod
     def __clean_path(path):
-        return path.replace("\n", " ").replace("\r", " ").replace("\t", " ")
+        return path.replace("\n", " ").replace("\r", " ").replace("\t", " ").strip()
 
-    def to_unix(self, path: str):
+    def to_unix(self, path: str, native: bool = False):
+        if native:
+            bottle_path = ManagerUtils.get_bottle_path(self.config)
+            path = path.replace("\\", "/")
+            path = path.replace(
+                path[0:2],
+                f"{bottle_path}/dosdevices/{path[0:2].lower()}"
+            )
+            return self.__clean_path(path)
         args = f"--unix '{path}'"
         res = self.launch(args=args, comunicate=True, action_name="--unix")
         return self.__clean_path(res)
