e7c607c68030d4b723c700035fd2ab9deb18692f
mirkobrombin <send@mirko.pm>
2022-05-19 22:55:52 +0200

    closes #1485

---
+++ src/backend/wine/regsvr32.py
@@ -0,0 +1,29 @@
+import os
+import uuid
+from typing import NewType
+
+from bottles.backend.logger import Logger  # pyright: reportMissingImports=false
+from bottles.backend.wine.wineprogram import WineProgram
+
+logging = Logger()
+
+
+class Regsvr32(WineProgram):
+    program = "Wine DLL Registration Server"
+    command = "regsvr32"
+
+    def register(self, dll: str):
+        args = f"/s {dll}"
+        return self.launch(args=args, comunicate=True, action_name="register")
+
+    def unregister(self, dll: str):
+        args = f"/s /u {dll}"
+        return self.launch(args=args, comunicate=True, action_name="unregister")
+
+    def register_all(self, dlls: list):
+        for dll in dlls:
+            self.register(dll)
+
+    def unregister_all(self, dlls: list):
+        for dll in dlls:
+            self.unregister(dll)
