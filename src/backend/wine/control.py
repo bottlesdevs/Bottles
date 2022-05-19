a1e84da3b11ff72f42729505a0d8e22b616d0bf4
mirkobrombin <send@mirko.pm>
2022-05-19 23:10:16 +0200

    closes #1490

--- src/backend/wine/control.py
+++ src/backend/wine/control.py
@@ -9,3 +9,16 @@ logging = Logger()
 class Control(WineProgram):
     program = "Wine Control Panel"
     command = "control"
+
+    def load_applet(self, name: str):
+        args = name
+        return self.launch(args=args, comunicate=True, action_name="load_applet")
+
+    def load_joystick(self):
+        return self.load_applet("joy.cpl")
+
+    def load_appwiz(self):
+        return self.load_applet("appwiz.cpl")
+
+    def load_inetcpl(self):
+        return self.load_applet("inetcpl.cpl")
