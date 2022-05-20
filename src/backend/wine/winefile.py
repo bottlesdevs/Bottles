ccebca873dacd66f7185426fc13b02c962dcc006
mirkobrombin <send@mirko.pm>
2022-05-20 12:48:29 +0200

    add winefile WineProgram

---
+++ src/backend/wine/winefile.py
@@ -0,0 +1,15 @@
+from typing import NewType
+
+from bottles.backend.logger import Logger  # pyright: reportMissingImports=false
+from bottles.backend.wine.wineprogram import WineProgram
+
+logging = Logger()
+
+
+class WineFile(WineProgram):
+    program = "Wine File Explorer"
+    command = "winefile"
+
+    def open_path(self, path: str = "C:\\\\"):
+        args = path
+        return self.launch(args=args, comunicate=True, action_name="open_path")
