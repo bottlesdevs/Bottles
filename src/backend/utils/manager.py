04c50cd6d60fc2a3bc8d98e27c819b61630c7e26
mirkobrombin <send@mirko.pm>
2022-05-22 15:07:18 +0200

    fix #1501

--- src/backend/utils/manager.py
+++ src/backend/utils/manager.py
@@ -14,6 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import shlex
 
 import gi
 import os
@@ -202,7 +203,7 @@ class ManagerUtils:
         with open(desktop_file, "w") as f:
             f.write(f"[Desktop Entry]\n")
             f.write(f"Name={program.get('name')}\n")
-            f.write(f"Exec={cmd} run -p '{program.get('name')}' -b '{config.get('Path')}'\n")
+            f.write(f"Exec={cmd} run -p {shlex.quote(program.get('name'))} -b '{config.get('Path')}'\n")
             f.write(f"Type=Application\n")
             f.write(f"Terminal=false\n")
             f.write(f"Categories=Application;\n")
