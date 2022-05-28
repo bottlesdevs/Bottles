e4fe9a248384d72584c442adf82a0ccfc21381a1
mirkobrombin <send@mirko.pm>
2022-05-28 21:40:28 +0200

    2022.5.28-trento-2 with Regsvr32 support

--- src/params.py
+++ src/params.py
@@ -2,8 +2,8 @@
 APP_NAME = "Bottles"
 APP_NAME_LOWER = APP_NAME.lower()
 APP_ID = "com.usebottles.bottles"
-VERSION = "2022.5.28-trento-1"
-VERSION_NUM = "2022.5.28-trento-1"
+VERSION = "2022.5.28-trento-2"
+VERSION_NUM = "2022.5.28-trento-2"
 
 # Internal settings not user editable
 ANIM_DURATION = 120
