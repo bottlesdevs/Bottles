d9e42db8e0a9c177e61636411e5f1828aec555c4
jannuary <27908024+jannuary@users.noreply.github.com>
2022-06-01 16:34:29 +0700

    Merge branch 'master'

--- src/backend/managers/manager.py
+++ src/backend/managers/manager.py
@@ -1135,7 +1135,7 @@ class Manager:
             wineboot.kill()
 
         if env:
-            while wineserver.is_alive():
+            while wineserver_status:
                 time.sleep(1)
 
             for prm in config["Parameters"]:
