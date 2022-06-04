c8275c129dbb3768675d903b898f4a59171b4492
mirkobrombin <send@mirko.pm>
2022-06-04 15:14:12 +0200

    use AdwWindow for LaunchOptionsDialog

--- src/dialogs/launchoptions.py
+++ src/dialogs/launchoptions.py
@@ -16,11 +16,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-from gi.repository import Gtk, GLib
+from gi.repository import Gtk, GLib, Adw
 
 
 @Gtk.Template(resource_path='/com/usebottles/bottles/dialog-launch-options.ui')
-class LaunchOptionsDialog(Gtk.Window):
+class LaunchOptionsDialog(Adw.Window):
     __gtype_name__ = 'LaunchOptionsDialog'
     __default_msg = _("Choose a script which should be executed after run.")
 
