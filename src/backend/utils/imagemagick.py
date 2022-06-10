a17b2611c95e892dfea8ba14380227574ab407db
mirkobrombin <send@mirko.pm>
2022-06-10 13:27:21 +0200

    improve ImageMagick assets listing and keep alpha

--- src/backend/utils/imagemagick.py
+++ src/backend/utils/imagemagick.py
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import subprocess
+
 
 class ImageMagick:
     def __init__(self, path: str):
@@ -29,31 +32,47 @@ class ImageMagick:
         return True
 
     def list_assets(self):
-        res = subprocess.check_output(['identify', self.path]).decode('utf-8').split('\n')
-        assets = []
+        cmd = f"identify '{self.path}'"
 
-        for r in res:
-            _r = r.split(' ')
+        try:
+            res = subprocess.check_output(['bash', "-c", cmd])
+        except:
+            return []
+
+        assets = []
+        for r in res.decode().split("\n"):
+            _r = r.replace(self.path, "").split()
             if len(_r) < 3:
                 continue
-            assets.append(int(_r[2].split("x")[0]))
+            try:
+                assets.append(int(_r[2].split("x")[0]))
+            except ValueError:
+                continue
         return assets
 
-    def convert(self, dest: str, asset_size: int = 256, resize: int = 0, flatten: bool = True):
+    def convert(self, dest: str, asset_size: int = 256, resize: int = 256, flatten: bool = True, fallback: bool = True):
         if not self.__validate_path(dest):
             raise FileExistsError("Destination path already exists")
 
-        if asset_size not in self.list_assets():
-            raise ValueError("Asset size not available")
+        assets = self.list_assets()
+        asset_index = -1
+        cmd = f"convert '{self.path}'"
 
-        asset_index = self.list_assets().index(asset_size)
-        cmd = f"convert {self.path}[{asset_index}]"
+        if asset_size not in assets:
+            if not fallback:
+                raise ValueError("Asset size not available")
+            if len(assets) > 0:
+                asset_size = max(assets)
+                asset_index = assets.index(asset_size)
+        else:
+            asset_index = assets.index(asset_size)
 
+        if asset_index != -1:
+            cmd = f"convert '{self.path}[{asset_index}]'"
         if flatten:
             cmd += " -flatten"
         if resize > 0:
             cmd += f" -thumbnail {resize}x{resize}"
 
-        cmd += f" {dest}"
-
+        cmd += f" -alpha on -background none '{dest}'"
         subprocess.Popen(["bash", "-c", cmd])
