d944a15c057ae71dc03df49442b9ab57ff63fd52
mirkobrombin <send@mirko.pm>
2022-06-20 17:33:37 +0200

    fix #1652

--- src/dialogs/envvars.py
+++ src/dialogs/envvars.py
@@ -56,7 +56,6 @@ class EnvVarEntry(Adw.EntryRow):
             value=self.get_text(),
             scope="Environment_Variables"
         )
-        self.set_text("")
 
     def __remove(self, *args):
         """
@@ -117,13 +116,12 @@ class EnvVarsDialog(Adw.Window):
         self.manager.update_config(
             config=self.config,
             key=env_name,
-            value="",
+            value="value",
             scope="Environment_Variables"
         )
-        _entry = EnvVarEntry(parent=self, env=[env_name, ""])
+        _entry = EnvVarEntry(parent=self, env=[env_name, "value"])
         GLib.idle_add(self.group_vars.add, _entry)
         self.entry_name.set_text("")
-        self.entry_name.set_show_apply_button(False)
 
     def __populate_vars_list(self):
         """
