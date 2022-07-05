a767abb5e39ddf23261029335d9cf07e288744e6
mirkobrombin <send@mirko.pm>
2022-07-05 18:13:42 +0200

    add QueueManager

---
+++ src/backend/managers/queue.py
@@ -0,0 +1,37 @@
+# queue.py
+#
+# Copyright 2020 brombinmirko <send@mirko.pm>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from gi.repository import GLib
+
+
+class QueueManager:
+    __queue = 0
+
+    def __init__(self, end_fn, add_fn=None):
+        self.__add_fn = add_fn
+        self.__end_fn = end_fn
+
+    def add_task(self):
+        self.__queue += 1
+        if self.__add_fn and self.__queue == 1:
+            GLib.idle_add(self.__add_fn)
+
+    def end_task(self):
+        self.__queue -= 1
+        if self.__queue <= 0:
+            GLib.idle_add(self.__end_fn)
