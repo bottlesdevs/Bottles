ebde7f3b784078dedac816c2ad40ad87d7c1c24a
TheEvilSkeleton <theevilskeleton@riseup.net>
2022-11-03 00:58:47 -0400

    remake path UI

--- bottles/frontend/views/preferences.py
+++ bottles/frontend/views/preferences.py
@@ -52,7 +52,7 @@ class PreferencesWindow(Adw.PreferencesWindow):
     list_nvapi = Gtk.Template.Child()
     list_latencyflex = Gtk.Template.Child()
     action_prerelease = Gtk.Template.Child()
-    action_bottles_path = Gtk.Template.Child()
+    btn_bottles_path = Gtk.Template.Child()
     action_steam_proton = Gtk.Template.Child()
     btn_bottles_path_reset = Gtk.Template.Child()
     label_bottles_path = Gtk.Template.Child()
@@ -107,7 +107,7 @@ class PreferencesWindow(Adw.PreferencesWindow):
         self.settings.connect('changed::dark-theme', self.__toggle_night)
         self.settings.connect('changed::release-candidate', self.__toggle_rc)
         self.settings.connect('changed::update-date', self.__toggle_update_date)
-        self.action_bottles_path.connect('activated', self.__choose_bottles_path)
+        self.btn_bottles_path.connect('activated', self.__choose_bottles_path)
         self.btn_bottles_path_reset.connect('clicked', self.__reset_bottles_path)
         self.btn_steam_proton_doc.connect('clicked', self.__open_steam_proton_doc)
 
