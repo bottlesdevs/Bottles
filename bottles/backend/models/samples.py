256b79b6777d5f205e95635fbfb91655926a4b5d
Kinsteen <pitiqui69@gmail.com>
2022-11-21 16:05:24 +0100

    Removed option in UI

--- bottles/backend/models/samples.py
+++ bottles/backend/models/samples.py
@@ -46,7 +46,6 @@ class Samples:
             "custom_dpi": 96,
             "renderer": "gl",
             "discrete_gpu": False,
-            "embedded_gstreamer": True,
             "virtual_desktop": False,
             "virtual_desktop_res": "1280x720",
             "pulseaudio_latency": False,
