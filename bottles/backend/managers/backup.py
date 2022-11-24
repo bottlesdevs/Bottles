a065d13f2055d7ef34d1d0704f4359b7b58e60e6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-24 07:15:50 +0000

    Adding tarfile member sanitization to extractall()

--- bottles/backend/managers/backup.py
+++ bottles/backend/managers/backup.py
@@ -154,7 +154,26 @@ class BackupManager:
 
             try:
                 with tarfile.open(path, "r:gz") as tar:
-                    tar.extractall(Paths.bottles)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, Paths.bottles)
                 import_status = True
             except (FileNotFoundError, PermissionError, tarfile.TarError):
                 import_status = False
