32681b2ed801607b5d640043d6f7c91a0c61f243
Kinsteen <pitiqui69@gmail.com>
2022-12-25 01:36:02 +0100

    feat: only update deps when changing pages

--- bottles/frontend/views/details.py
+++ bottles/frontend/views/details.py
@@ -115,6 +115,7 @@ class DetailsView(Adw.Bin):
         self.set_title(self.__pages[page]['title'], self.__pages[page]['description'])
         if page == "dependencies":
             self.set_actions(self.view_dependencies.actions)
+            self.view_dependencies.update(config=self.config)
         elif page == "versioning":
             self.set_actions(self.view_versioning.actions)
         elif page == "installers":
@@ -189,7 +190,6 @@ class DetailsView(Adw.Bin):
         self.view_bottle.set_config(config=config)
         self.view_preferences.set_config(config=config)
         self.view_taskmanager.set_config(config=config)
-        self.view_dependencies.update(config=config)
         self.view_installers.update(config=config)
         self.view_versioning.update(config=config)
 
