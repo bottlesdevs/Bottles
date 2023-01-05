0a9cac4718cc9fbfd4b7f11f5c267e8980b8142e
TheEvilSkeleton <theevilskeleton@riseup.net>
2023-01-04 21:41:27 -0500

    Port other sections

--- bottles/frontend/main.py
+++ bottles/frontend/main.py
@@ -35,6 +35,8 @@ from gi.repository import Gtk, Gio, Gdk, GLib, GObject, Adw
 from bottles.frontend.params import *
 from bottles.backend.logger import Logger
 from bottles.frontend.windows.main_window import MainWindow
+from bottles.frontend.views.preferences import PreferencesWindow
+from bottles.backend.health import HealthChecker
 
 logging = Logger()
 
@@ -86,14 +88,64 @@ class Bottles(Adw.Application):
         )
         self.__create_action('quit', self.__quit, ['<primary>q'])
         self.__create_action('quit', self.__quit, ['<primary>w'])
-        # self.__create_action('about', self.__about_action)
-        # self.__create_action('open', self.__import, ['<primary>i'])
-        # self.__create_action('preferences', self.__preferences)
+        self.__create_action('about', self.__show_about_window)
+        self.__create_action('import', self.__show_importer_view, ['<primary>i'])
+        self.__create_action('preferences', self.__show_preferences)
         self.__create_action('help', self.__help, ['F1'])
-        # self.__create_action('open', self.__import, ['<primary>i'])
-
-        self.win = None
         self.__register_arguments()
+
+    def __show_preferences(self, *args):
+        preferences_window = PreferencesWindow(self.win)
+        preferences_window.present()
+
+    def __show_importer_view(self, widget=False, *args):
+        self.win.main_leaf.set_visible_child(self.win.page_importer)
+
+    def __show_about_window(self, *_args):
+        builder = Gtk.Builder.new_from_resource("/com/usebottles/bottles/about.ui")
+        about_window = builder.get_object("about_window")
+        about_window.set_debug_info(HealthChecker().get_results(plain=True))
+        about_window.add_link(_("Donate"), "https://usebottles.com/funding")
+        about_window.set_version(APP_VERSION)
+        about_window.set_application_name(APP_NAME)
+        about_window.set_application_icon(APP_ICON)
+        about_window.add_acknowledgement_section(
+            _("Third-Party Libraries and Special Thanks"),
+            [
+                "DXVK https://github.com/doitsujin/dxvk",
+                "VKD3D https://github.com/HansKristian-Work/vkd3d-proton",
+                "DXVK-NVAPI https://github.com/jp7677/dxvk-nvapi",
+                "LatencyFleX https://github.com/ishitatsuyuki/LatencyFleX",
+                "MangoHud https://github.com/flightlessmango/MangoHud",
+                "AMD FidelityFX™ Super Resolution https://www.amd.com/en/technologies/fidelityfx-super-resolution",
+                "vkBasalt https://github.com/DadSchoorse/vkBasalt",
+                "vkbasalt-cli https://gitlab.com/TheEvilSkeleton/vkbasalt-cli",
+                "GameMode https://github.com/FeralInteractive/gamemode",
+                "Gamescope https://github.com/Plagman/gamescope",
+                "OBS Vulkan/OpenGL capture https://github.com/nowrep/obs-vkcapture",
+                "Wine-TKG https://github.com/Frogging-Family/wine-tkg-git",
+                "Proton https://github.com/ValveSoftware/proton",
+                "Wine-GE https://github.com/GloriousEggroll/wine-ge-custom",
+                "Wine https://www.winehq.org",
+                "orjson https://github.com/ijl/orjson",
+                "libadwaita https://gitlab.gnome.org/GNOME/libadwaita",
+                "icoextract https://github.com/jlu5/icoextract",
+                "vmtouch https://github.com/hoytech/vmtouch",
+                "FVS https://github.com/mirkobrombin/FVS"
+            ]
+        )
+        about_window.add_acknowledgement_section(
+            _("Sponsored and Funded by"),
+            [
+                "JetBrains https://www.jetbrains.com/?from=bottles",
+                "GitBook https://www.gitbook.com/?ref=bottles",
+                "Linode https://www.linode.com/?from=bottles",
+                "Appwrite https://appwrite.io/?from=bottles",
+                "Community ❤️ https://usebottles.com/funding"
+            ]
+        )
+        about_window.set_transient_for(self.win)
+        about_window.present()
 
     def __register_arguments(self):
         """
@@ -219,7 +271,6 @@ class Bottles(Adw.Application):
         See: __register_actions()
         """
         Adw.Application.do_startup(self)
-        self.__register_actions()
 
     def do_activate(self):
         """
@@ -276,8 +327,6 @@ class Bottles(Adw.Application):
         action = Gio.SimpleAction.new(name, param)
         action.connect("activate", callback)
         self.add_action(action)
-        if shortcuts:
-            self.set_accels_for_action(f"app.{name}", shortcuts))
 
 GObject.threads_init()
 
