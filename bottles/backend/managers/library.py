9f1085e20aab4f1ce4f18c72ab4f524c721eb13e
StoneMoe <lakechan96@gmail.com>
2023-01-18 13:28:40 +0800

    fix typo in cbe1dc760b60fc4086858a88313c8a3de181be31

--- bottles/backend/managers/library.py
+++ bottles/backend/managers/library.py
@@ -79,21 +79,20 @@ class LibraryManager:
         self.__library[_uuid] = data
         self.save_library()
 
-    def download_thumbnail(self, uuid: str, config: dict):
-        if not self.__library.get(uuid):
+    def download_thumbnail(self, _uuid: str, config: dict):
+        if not self.__library.get(_uuid):
             logging.warning(f'Entry not found in library, can\'t download thumbnail: {_uuid}')
             return False
 
-        data = self.__library.get(uuid)
+        data = self.__library.get(_uuid)
         value = SteamGridDBManager.get_game_grid(data['name'], config)
 
         if not value:
             return False
 
-        self.__library[uuid]['thumbnail'] = value
+        self.__library[_uuid]['thumbnail'] = value
         self.save_library()
         return True
-
 
     def __already_in_library(self, data: dict):
         """
