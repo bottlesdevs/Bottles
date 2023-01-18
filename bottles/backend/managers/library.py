c9e1513deddfd856f9c2515283fe73f77cd4d842
Kinsteen <me@kinsteen.fr>
2023-01-18 16:44:56 +0100

    fix: Remove Add to Library button if already in library (#2561)

--- bottles/backend/managers/library.py
+++ bottles/backend/managers/library.py
@@ -37,9 +37,9 @@ class LibraryManager:
     __library: dict = {}
 
     def __init__(self):
-        self.load_library()
+        self.load_library(silent=True)
 
-    def load_library(self):
+    def load_library(self, silent=False):
         """
         Loads data from the library.yml file.
         """
@@ -59,7 +59,7 @@ class LibraryManager:
             if "id" not in v:
                 del self.__library[k]
 
-        self.save_library()
+        self.save_library(silent=silent)
 
     def add_to_library(self, data: dict, config: dict):
         """
@@ -115,14 +115,15 @@ class LibraryManager:
             return
         logging.warning(f'Entry not found in library, nothing to remove: {_uuid}')
 
-    def save_library(self):
+    def save_library(self, silent=False):
         """
         Saves the library.yml file.
         """
         with open(self.library_path, 'w') as library_file:
             yaml.dump(self.__library, library_file)
             
-        logging.info(f'Library saved')
+        if not silent:
+            logging.info(f'Library saved')
 
     def get_library(self):
         """
