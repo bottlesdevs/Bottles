72ccd4a9eb5a6b265028cd2d169fdaf7aeedf3d8
StoneMoe <lakechan96@gmail.com>
2023-01-29 21:12:29 +0800

    Decoupling ComponentManager & Downloader

--- bottles/backend/state.py
+++ bottles/backend/state.py
@@ -1,7 +1,8 @@
 import dataclasses
 from enum import Enum
+from gettext import gettext as _
 from threading import Lock as PyLock, Event as PyEvent
-from typing import Dict, Callable, Union
+from typing import Dict, Callable, Optional, Union, Protocol
 from uuid import UUID, uuid4
 
 
@@ -15,21 +16,61 @@ class Status(Enum):
     FAILED = "failed"
 
 
+class TaskStreamUpdateHandler(Protocol):
+    def __call__(self, received_size: int = 0, total_size: int = 0, status: Status = None) -> None: ...
+
+
 @dataclasses.dataclass
 class Task:
-    title: str = ""
-    description: str = ""
+    _task_id: Optional[UUID] = None  # should only be set by TaskManager
+    title: str = "Task"
+    _subtitle: str = ""
     hidden: bool = False  # hide from UI
     cancellable: bool = False
 
+    @property
+    def task_id(self) -> UUID:
+        return self._task_id
 
-class LockService:
+    @task_id.setter
+    def task_id(self, value: UUID):
+        if self._task_id is not None:
+            raise NotImplementedError("Invalid usage, Task.task_id should only set once")
+        self._task_id = value
+
+    @property
+    def subtitle(self) -> str:
+        return self._subtitle
+
+    @subtitle.setter
+    def subtitle(self, value: str):
+        self._subtitle = value
+        # TODO: sync signal
+
+    def stream_update(self, received_size: int = 0, total_size: int = 0, status: Status = None):
+        """This is a default subtitle updating handler for streaming downloading progress"""
+        match status:
+            case Status.DONE, Status.FAILED:
+                TaskManager.remove_task(self)
+                return
+            case _:
+                pass
+
+        if total_size == 0 and received_size == 0:
+            self.subtitle = _("Calculating…")
+            return
+
+        percent = int(received_size / total_size * 100)
+        self.subtitle = f"{percent}%"
+
+
+class LockManager:
     _LOCKS: Dict[str, PyLock] = {}  # {"lock_name": Lock}
 
     @classmethod
     def lock(cls, name: Locks):
         """decorator, used for mutex locking the decorated function"""
-        lock = cls._LOCKS.setdefault(name.value, PyLock())
+        lock = cls.get_lock(name)
 
         def func_wrapper(func: Callable):
             def wrapper(*args, **kwargs):
@@ -51,25 +92,31 @@ class EventManager:
     _EVENTS: Dict[str, PyEvent] = {}  # {"event_name": Event}
 
 
-class StatusManager:
-    _STATUS: Dict[str, Status] = {}  # {"status_name": status}
-
-
 class TaskManager:
-    _TASKS: Dict[str, Task]  # {"task_id(uuid4 str)": Task}
+    """Long-running tasks are registered here, for tracking and display them on UI"""
+    _TASKS: Dict[UUID, Task] = {}  # {UUID4: Task}
+
+    @classmethod
+    def get_task(cls, task_id: UUID) -> Optional[Task]:
+        return cls._TASKS.get(task_id)
 
     @classmethod
     def add_task(cls, task: Task) -> UUID:
         """register a running task to TaskManager"""
         uniq = uuid4()
-        cls._TASKS[str(uniq)] = task
+        task.task_id = uniq
+        cls._TASKS[uniq] = task
+        # TODO: sync signal
         return uniq
 
     @classmethod
-    def remove_task(cls, uuid: Union[str, UUID]):
-        cls._TASKS.pop(str(uuid))
+    def remove_task(cls, task: Union[UUID, Task]):
+        if isinstance(task, Task):
+            task = task.task_id
+        cls._TASKS.pop(task)
+        # TODO: sync signal
 
 
-class State(LockService, EventManager, StatusManager, TaskManager):
+class State(LockManager, EventManager, TaskManager):
     """Unified State Management"""
     pass
