53453f719a08f43c2d4b54c5bba3a033ce2eacbc
axtloss <axtlos@getcryst.al>
2023-05-07 18:35:00 +0200

    Block the use of GTK_THEME in bottles

--- bottles/frontend/bottles.py
+++ bottles/frontend/bottles.py
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+os.environ.pop('GTK_THEME')
 import sys
 import signal
 import gettext
