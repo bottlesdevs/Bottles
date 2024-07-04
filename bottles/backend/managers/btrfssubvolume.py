07385f7a30ae396125790661e006a60e1a790cf0
Johannes Röhl <joroehl@gmx.de>
2024-07-04 13:25:22 +0200

    BottleSnapshotsHandle class

--- bottles/backend/managers/btrfssubvolume.py
+++ bottles/backend/managers/btrfssubvolume.py
@@ -1,11 +1,92 @@
 import os
 import os.path
+import shutil
 import btrfsutil
 
 # Internal subvolumes created at initialization time:
 _internal_subvolumes = [
     "cache",
     ]
+
+def _delete_subvolume(path):
+    try:
+        btrfsutil.delete_subvolume(path)
+    except btrfsutil.BtrfsUtilError as error:
+        if not error.btrfsutilerror() == btrfsutil.ERROR_SNAP_DESTROY_FAILED or not issubclass(error, PermissionError):
+            raise
+        try:
+            # Try to delete the subvolume as a normal directory tree. This is
+            # in particular needed, when the btrfs filesystem is not mounted
+            # with 'user_subvol_rm_allowed' option.
+            btrfsutil.set_subvolume_read_only(path, False)
+            shutil.rmtree(path)
+        except Exception as e:
+            # Raise the first error with some appended notes
+            error.add_note(f"Subvolume path: '{path}' ")
+            error.add_note(f"Fallback to 'shutil.rmtree()' failed with: '{e}'")
+            raise error
+
+class BottleSnapshotsHandle:
+    """Handle the snapshots of a single bottle created as btrfs subvolume.
+    """
+
+    # TODO delete the snapshots, when the bottle get's deleted
+
+    def __init__(self, bottle_path):
+        """Internal should not be called directly.
+
+        Use BtrfsSubvolumeManager.create_bottle_snapshots_handle() to
+        potentially create an instance.
+        """
+        self._bottle_path = bottle_path
+        bottles_dir, bottle_name = os.path.split(bottle_path)
+        self._snapshots_directory = os.path.join(bottles_dir, "BottlesSnapshots", bottle_name)
+
+        self._snapshots = {}
+        if os.path.exists(self._snapshots_directory):
+            for snapshot in os.listdir(self._snapshots_directory):
+                if not btrfsutil.is_subvolume(os.path.join(self._snapshots_directory, snapshot)):
+                    continue
+                snapshot_id, separator, description = snapshot.partition("_")
+                if empty(separator):
+                    continue
+                self._snapshots[int(snapshot_id)] = description
+
+    def snapshots(self) -> dict:
+        """A dictionary of all available snapshots.
+
+        Returns a dictionary from snapshot ID (int) to description (str).
+        """
+        return self._snapshots.copy()
+
+    def _snapshot_path(self, snapshot_id: int, description: str):
+        return os.path.join(self._snapshots_directory, f"{snapshot_id}_{description}")
+
+    def _snapshot_path(self, snapshot_id: int):
+        return self._snapshot_path(snapshot_id, self._snapshots[snapshot_id])
+
+    def create_snapshot(self, description: str):
+        snapshot_id = max(self._snapshots.get_keys(), default=-1) + 1
+        snapshot_path = self._snapshot_path(snapshot_id, description)
+        os.makedirs(self._snapshots_directory, exist_ok=True)
+        btrfsutil.create_snapshot(self._bottle_path, snapshot_path, read_only=True)
+        self._snapshots[snapshot_id] = description
+
+    def set_state(self, snapshot_id: int):
+        """Restore the bottle state from a snapshot.
+        """
+        tmp_bottle_path = f"{self._bottle_path}-tmp"
+        snapshot_path = self._snapshot_path(snapshot_id)
+        os.rename(self._bottle_path, tmp_bottle_path)
+        btrfsutil.create_snapshot(snapshot_path, self._bottle_path, read_only=False)
+        for internal_subvolume in _internal_subvolumes:
+            source_path = os.path.join(tmp_bottle_path, internal_subvolume)
+            if not os.path.exists(source_path) or not btrfsutil.is_subvolume(source_path):
+                continue
+            destination_path = os.path.join(self._bottle_path, internal_subvolume)
+            os.rmdir(destination_path)
+            os.rename(source_path, destination_path)
+        _delete_subvolume(tmp_bottle_path)
 
 class BtrfsSubvolumeManager:
     """
@@ -23,8 +104,8 @@ class BtrfsSubvolumeManager:
         """Create bottle as btrfs subvolume.
 
         Creates the directory 'bottle_path' as btrfs subvolume and internal
-        subvolumes inside of it. Returns True on success and False on failure.
-        In particular it fails, if the filesystem is not btrfs.
+        subvolumes inside of it. Returns True on success and False, if the
+        filesystem is not btrfs. For other failures an exception is raised.
         """
 
         os.makedirs(os.path.dirname(bottle_path), exist_ok=True)
@@ -39,3 +120,17 @@ class BtrfsSubvolumeManager:
             return False
         else:
             return True
+
+    @staticmethod
+    def create_bottle_snapshots_handle(bottle_path):
+        """Try to create a bottle snapshots handle.
+
+        Checks if the bottle states can be stored as btrfs snapshots and if no
+        states have been stored by FVS versioning system. Returns
+        BottleSnapshotsHandle, if checks succeed, None otherwise.
+        """
+        if not btrfsutil.is_subvolume(bottle_path):
+            return None
+        if os.path.exists(os.path.join(bottle_path, ".fvs")):
+            return None
+        return BottleSnapshotsHandle(bottle_path)
