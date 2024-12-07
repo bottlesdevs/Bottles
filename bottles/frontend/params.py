01b0abcb6becafb0e40d4e773d152b2af0876494
Hari Rana <theevilskeleton@riseup.net>
2024-12-07 15:15:42 -0500

    build: Port devel option to combo options
    Introduce `profile` option, which allows us to remove the
    `.conf` files.

--- bottles/frontend/params.py
+++ bottles/frontend/params.py
@@ -4,7 +4,7 @@ APP_NAME_LOWER = APP_NAME.lower()
 APP_ID = "com.usebottles.bottles"
 APP_VERSION = "@APP_VERSION@"
 APP_ICON = "@APP_ID@"
-BUILD_TYPE = "prod"
+PROFILE = "@PROFILE@"
 
 # Internal settings not user editable
 ANIM_DURATION = 120
