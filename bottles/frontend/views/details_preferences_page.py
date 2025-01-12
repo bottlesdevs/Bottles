854c31a35ae1dde0caa60ad7eff0ddee2c683b0a
Hari Rana <theevilskeleton@riseup.net>
2025-01-12 18:45:56 -0500

    fsr-dialog: Rename files and add to POTFILES

--- bottles/frontend/views/details_preferences_page.py
+++ bottles/frontend/views/details_preferences_page.py
@@ -48,7 +48,7 @@ from bottles.frontend.windows.dll_overrides_dialog import DLLOverridesDialog
 from bottles.frontend.windows.drives_dialog import DrivesDialog
 from bottles.frontend.windows.envvars import EnvironmentVariablesDialog
 from bottles.frontend.windows.exclusion_patterns_dialog import ExclusionPatternsDialog
-from bottles.frontend.windows.fsr import FsrDialog
+from bottles.frontend.windows.fsr_dialog import FsrDialog
 from bottles.frontend.windows.gamescope_dialog import GamescopeDialog
 from bottles.frontend.windows.mangohud import MangoHudDialog
 from bottles.frontend.windows.protonalert import ProtonAlertDialog
