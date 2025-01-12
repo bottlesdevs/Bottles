8fbea9360bfa10b8f7c0f19d5a7d85b859fa015e
Hari Rana <theevilskeleton@riseup.net>
2025-01-12 15:27:43 -0500

    dll-overrides-dialog: Rename files

--- bottles/frontend/views/details_preferences_page.py
+++ bottles/frontend/views/details_preferences_page.py
@@ -44,7 +44,7 @@ from bottles.backend.utils.threading import RunAsync
 from bottles.backend.wine.regkeys import RegKeys
 from bottles.frontend.utils.gtk import GtkUtils
 from bottles.frontend.windows.display_dialog import DisplayDialog
-from bottles.frontend.windows.dlloverrides import DLLOverridesDialog
+from bottles.frontend.windows.dll_overrides_dialog import DLLOverridesDialog
 from bottles.frontend.windows.drives import DrivesDialog
 from bottles.frontend.windows.envvars import EnvironmentVariablesDialog
 from bottles.frontend.windows.exclusionpatterns import ExclusionPatternsDialog
