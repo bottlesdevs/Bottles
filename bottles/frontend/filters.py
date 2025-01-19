c9bef06b5725c4ec8fb74232cfba45b0460b29dc
EmoonX <emoon@emoon.dev>
2025-01-18 22:40:35 -0500

    filters: Use `__set_filter` private method to every function
    Co-authored-by: Hari Rana <theevilskeleton@riseup.net>

--- bottles/frontend/filters.py
+++ bottles/frontend/filters.py
@@ -20,36 +20,22 @@ from gi.repository import Gio, GObject, Gtk
 
 
 def add_executable_filters(dialog):
-    filter = Gtk.FileFilter()
-    filter.set_name(_("Supported Executables"))
     # TODO: Investigate why `filter.add_mime_type(...)` does not show filter in all distributions.
     # Intended MIME types are:
     #   - `application/x-ms-dos-executable`
     #   - `application/x-msi`
-    filter.add_pattern("*.exe")
-    filter.add_pattern("*.msi")
-
-    dialog.add_filter(filter)
+    __set_filter(dialog, _("Supported Executables"), ["*.exe", "*.msi"])
 
 
 def add_yaml_filters(dialog):
-    filter = Gtk.FileFilter()
-    filter.set_name("YAML")
     # TODO: Investigate why `filter.add_mime_type(...)` does not show filter in all distributions.
     # Intended MIME types are:
     #   - `application/yaml`
-    filter.add_pattern("*.yml")
-    filter.add_pattern("*.yaml")
-
-    dialog.add_filter(filter)
+    __set_filter(dialog, "YAML", ["*.yaml", "*.yml"])
 
 
 def add_all_filters(dialog):
-    filter = Gtk.FileFilter()
-    filter.set_name(_("All Files"))
-    filter.add_pattern("*")
-
-    dialog.add_filter(filter)
+    __set_filter(dialog, _("All Files"), ["*"])
 
 
 def __set_filter(dialog: GObject.Object, name: str, patterns: list[str]):
