cef2cc463193377cd3dda3c8cec047edce0e3966
Hari Rana <theevilskeleton@riseup.net>
2025-01-20 22:25:17 -0500

    frontend: Remove CLI

--- bottles/tests/backend/manager/test_manager.py
+++ bottles/tests/backend/manager/test_manager.py
@@ -5,11 +5,9 @@ from bottles.backend.utils.gsettings_stub import GSettingsStub
 
 
 def test_manager_is_singleton():
-    assert Manager(is_cli=True) is Manager(
-        is_cli=True
-    ), "Manager should be singleton object"
-    assert Manager(is_cli=True) is Manager(
-        g_settings=GSettingsStub(), is_cli=True
+    assert Manager() is Manager(), "Manager should be singleton object"
+    assert Manager() is Manager(
+        g_settings=GSettingsStub()
     ), "Manager should be singleton even with different argument"
 
 
