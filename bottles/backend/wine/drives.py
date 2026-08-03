f89398baa9927eab3bc39e4f654461149b3cefe9
mirkobrombin <brombin94@gmail.com>
2026-08-03 10:18:28 +0200

    fix[closes #3453]: eject removable drives

--- bottles/backend/wine/drives.py
+++ bottles/backend/wine/drives.py
@@ -31,6 +31,13 @@ class Drives:
             return self.get_all().get(letter)
         return None
 
+    def is_ejectable(self, letter: str) -> bool:
+        """Return whether the drive has a Wine device link."""
+        device_link = os.path.join(
+            self.dosdevices_path, f"{letter.lower().rstrip(':')}::"
+        )
+        return os.path.islink(device_link)
+
     def set_drive_path(self, letter: str, path: str):
         """Change a drives path in the bottle"""
         letter = f"{letter}:".lower()
